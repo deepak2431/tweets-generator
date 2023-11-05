c309cd1a6f56c19065755460575ead20c3cb4396
deepak2431 <deepakdk2431@gmail.com>
2023-11-05 23:24:44 +0530

    implemented content generator

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,46 @@
 import React, { useState } from "react";
+import OpenAI from "openai";
 
 import SubmitButton from "./components/SubmitButton";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import SearchInput from "./components/SearchInput";
+import TweetCard from "./components/TweetCard";
+
+const openai = new OpenAI({
+  apiKey: "",
+  dangerouslyAllowBrowser: true,
+});
 
 const Home = () => {
   const [keywords, setKeywords] = useState("");
+  const [tweet, setTweet] = useState("");
+  const [showInput, setShowInput] = useState(true);
+
+  const generateTweet = async () => {
+    const response = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content: `Generate a viral tweet using the keywords: ${keywords}`,
+        },
+      ],
+      model: "gpt-3.5-turbo",
+    });
+
+    const generatedTweet = response.choices[0].message.content;
+    if(generatedTweet) {
+      setTweet(generatedTweet);
+    }
+    setShowInput(false);
+  };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setKeywords(e.target.value);
   };
 
-  const handleInputSubmit = () => {
-    console.log(keywords);
+  const handleInputSubmit = async () => {
+    await generateTweet();
   };
 
   return (
@@ -21,18 +48,21 @@ const Home = () => {
       <Header />
       <div className="content">
         <h2>Create Viral Tweet Content With Keywords</h2>
+        {tweet && <TweetCard tweetContent={tweet} />}
       </div>
-      <div className="input_container">
-        <SearchInput
-          placeHolder="Enter Keywords"
-          value={keywords}
-          handleChange={handleInputChange}
-        />
-        <SubmitButton
-          buttonText="Generate Tweet"
-          handleClick={handleInputSubmit}
-        />
-      </div>
+      {showInput && (
+        <div className="input_container">
+          <SearchInput
+            placeHolder="Enter Keywords"
+            value={keywords}
+            handleChange={handleInputChange}
+          />
+          <SubmitButton
+            buttonText="Generate Tweet"
+            handleClick={handleInputSubmit}
+          />
+        </div>
+      )}
       <Footer />
     </div>
   );
